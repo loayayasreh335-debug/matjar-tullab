db3fda79eaab0b7cf4780a33122e206038b2ae30
loayayasreh335-debug <loayayasreh335@gmail.com>
2026-07-28 21:49:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ app.post('/api/items', async (req, res) => {
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-
+});
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
